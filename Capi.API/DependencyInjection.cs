219bbd3017f36cdd8376f4bcf51a87a260185428
Nikolay Syryaev <zoronbet@gmail.com>
2025-09-15 01:11:25 +0400

    Добавьте файлы проекта.

---
+++ Capi.API/DependencyInjection.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Template.Capi.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+    
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            //if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapGet("/", () => "Hello World!");
+
+            return app;
+        }
+    }
+}
---
+++ Capi.API/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Template.Capi.API;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddApiServices(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseApiServices();
+
+app.Run();
