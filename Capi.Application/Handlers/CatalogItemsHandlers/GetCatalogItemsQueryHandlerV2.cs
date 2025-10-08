31136d24ff47c56106201b6ed750809ce47608a4
Nikolay Syryaev <zoronbet@gmail.com>
2025-10-09 02:22:59 +0400

    Добавлена сортировка при выборке товаров

--- Capi.Application/Handlers/CatalogItemsHandlers/GetCatalogItemsQueryHandlerV2.cs
+++ Capi.Application/Handlers/CatalogItemsHandlers/GetCatalogItemsQueryHandlerV2.cs
@@ -14,15 +14,38 @@ public class GetCatalogItemsQueryHandlerV2(ICatalogItemRepository catalogItemRep
     {
         var allItems = await catalogItemRepository.GetAllCatalogItemAsync(ct);
 
+        var brandId = query.Args.BrandId;
+        if (brandId is not null)
+            allItems.Where(e => e.Brand?.Id == brandId);
+
+        var categoryId = query.Args.CategoryId;
+        if (categoryId is not null)
+            allItems.Where(e => e.Category?.Id == categoryId);
+
+        var search = query.Args.Search;
+        if (!string.IsNullOrWhiteSpace(search))
+            allItems.Where(e => e.Title != null && e.Title.Contains(search, StringComparison.OrdinalIgnoreCase));
+
+        if (string.IsNullOrWhiteSpace(query.Args.Sort))
+        {
+            allItems = query.Args.Sort.ToLower() switch
+            {
+                "price_desc" => allItems.OrderByDescending(x => x.Price),
+                "price_asc" => allItems.OrderBy(x => x.Price),
+                "title_desc" => allItems.OrderByDescending(x => x.Title),
+                "title_asc" => allItems.OrderBy(x => x.Title),
+            };
+        }
+
         var count = allItems.Count();
         var items = allItems
-            .Skip((query.PageIndex - 1) * query.PageSize)
-            .Take(query.PageSize)
+            .Skip((query.Args.PageIndex - 1) * query.Args.PageSize)
+            .Take(query.Args.PageSize)
             .ToList();
 
         var pagination = new Pagination<CatalogItem>(
-            PageIndex: query.PageIndex,
-            PageSize: query.PageSize,
+            PageIndex: query.Args.PageIndex,
+            PageSize: query.Args.PageSize,
             TotalCount: count,
             Items: items);
 
--- Capi.Application/Queries/CatalogItemsQueries/GetCatalogItemsQueryV2.cs
+++ Capi.Application/Queries/CatalogItemsQueries/GetCatalogItemsQueryV2.cs
@@ -1,6 +1,7 @@
 ﻿using Capi.Application.Responses.CatalogItemResponses;
+using Capi.Domain.Specifications;
 using MediatR;
 
 namespace Capi.Application.Queries.CatalogItemsQueries;
 
-public record GetCatalogItemsQueryV2(int PageIndex, int PageSize) : IRequest<GetCatalogItemsResultV2>;
+public record GetCatalogItemsQueryV2(QueryArgs Args) : IRequest<GetCatalogItemsResultV2>;
